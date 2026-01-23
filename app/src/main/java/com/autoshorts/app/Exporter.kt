c10c48759c131a386ae2a78c1fb8c7d8defc8d5e
ajomlst77 <mailngedit@gmail.com>
2026-01-23 20:24:08 +0700

    Update Exporter.kt

--- app/src/main/java/com/autoshorts/app/Exporter.kt
+++ app/src/main/java/com/autoshorts/app/Exporter.kt
@@ -3,15 +3,6 @@ package com.autoshorts.app
 object Exporter {
 
     fun export(meta: VideoMeta): String {
-        return """
-            {
-              "videoUri": "${meta.videoUri}",
-              "width": ${meta.width},
-              "height": ${meta.height},
-              "rotation": ${meta.rotation},
-              "mimeType": "${meta.mimeType}",
-              "fileSizeBytes": ${meta.fileSizeBytes}
-            }
-        """.trimIndent()
+        return "EXPORT_OK"
     }
 }
