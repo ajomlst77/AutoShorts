bbaa10bd26a5929177ebccbce50aed0c10f95228
ajomlst77 <mailngedit@gmail.com>
2026-01-23 18:53:04 +0700

    Update VideoAnalyzer.kt

--- app/src/main/java/com/autoshorts/app/VideoAnalyzer.kt
+++ app/src/main/java/com/autoshorts/app/VideoAnalyzer.kt
@@ -3,5 +3,10 @@ package com.autoshorts.app
 import android.net.Uri
 
 data class VideoMeta(
-    val videoUri: Uri
+    val videoUri: Uri,
+    val width: Int = 0,
+    val height: Int = 0,
+    val rotation: Int = 0,
+    val mimeType: String = "",
+    val fileSizeBytes: Long = 0L
 )
