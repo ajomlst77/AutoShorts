1e3bf426ecd254fe102c36a846c5bf5126989e00
ajomlst77 <mailngedit@gmail.com>
2026-01-24 21:04:52 +0700

    Update VideoAnalyzer.kt

--- app/src/main/java/com/autoshorts/app/VideoAnalyzer.kt
+++ app/src/main/java/com/autoshorts/app/VideoAnalyzer.kt
@@ -1,54 +1,10 @@
 package com.autoshorts.app
 
-import android.content.Context
-import android.media.MediaMetadataRetriever
-import android.net.Uri
-
-object VideoAnalyzer {
-
-    fun analyze(context: Context, videoUri: Uri): VideoMeta {
-        val retriever = MediaMetadataRetriever()
-        var width = 0
-        var height = 0
-        var rotation = 0
-        var mime = ""
-        var sizeBytes = 0L
-
-        try {
-            retriever.setDataSource(context, videoUri)
-
-            width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH)
-                ?.toIntOrNull() ?: 0
-
-            height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT)
-                ?.toIntOrNull() ?: 0
-
-            rotation = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_ROTATION)
-                ?.toIntOrNull() ?: 0
-
-            mime = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_MIMETYPE) ?: ""
-        } catch (_: Exception) {
-            // ignore
-        } finally {
-            try { retriever.release() } catch (_: Exception) {}
-        }
-
-        // file size dari ContentResolver
-        try {
-            context.contentResolver.openAssetFileDescriptor(videoUri, "r")?.use { afd ->
-                sizeBytes = afd.length
-            }
-        } catch (_: Exception) {
-            // ignore
-        }
-
-        return VideoMeta(
-            videoUri = videoUri.toString(),
-            width = width,
-            height = height,
-            rotation = rotation,
-            mimeType = mime,
-            fileSizeBytes = sizeBytes
-        )
-    }
-}
+data class VideoMeta(
+    val videoUri: String,
+    val width: Int,
+    val height: Int,
+    val rotation: Int,
+    val mimeType: String,
+    val fileSizeBytes: Long
+)
